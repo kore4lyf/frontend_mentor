bbe0087febcaea8b9877083eb4567ec53a13f661
Korede Faleye <akfaleye@gmail.com>
2023-05-11 10:30:05 +0100

    article-preview-component: Remove overflow: hidden on card

--- article-preview-component/script/main.js
+++ article-preview-component/script/main.js
@@ -8,8 +8,11 @@ const miniSocials = document.querySelector(".mini-socials");
 share.addEventListener("click", () => {
     if(window.screen.width >= 768) {
         miniSocials.classList.toggle("hide");
+        socials.classList.add("hide");
+        cardProfile.classList.remove("hide");
+        cardProfileContainer.style.backgroundColor = "#fff";
     } else {
-
+        miniSocials.classList.add("hide");
         // Toggle visibility of element with "card-profile-container" class 
         if(cardProfile.classList.toggle("hide")) {
             cardProfileContainer.style.backgroundColor = "#48556a";
@@ -19,6 +22,5 @@ share.addEventListener("click", () => {
         
         // Toggle visibility of element with "social" class
         socials.classList.toggle("hide");
-        console.log(window.screen.width >= 768);
     }
 });
