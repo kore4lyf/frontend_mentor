a66a02b3dbe3e5af3a00e3d57017dd71ff99aa55
Korede Faleye <akfaleye@gmail.com>
2023-09-19 07:53:07 +0100

    Set card width

--- interactive-rating-component/style/tailwindcss/tailwind.config.js
+++ interactive-rating-component/style/tailwindcss/tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["../../*.html", "../../script/*.js"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+        primary: "hsl(25, 97%, 53%)",
+        litegray: "hsl(217, 12%, 63%)",
+        medgray: "hsl(216, 12%, 54%)",
+        darkblue: "hsl(213, 19%, 18%)",
+        verydarkblue: "hsl(216, 12%, 8%)"
+      },
+      fontFamily: {
+        Overpass: ["Overpass", "sans-serif"],
+      }
+    },
   plugins: [],
+  }
 }
-
