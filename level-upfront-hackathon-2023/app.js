d6385a126d393bf099292a1556b8dca8e3a06333
Korede Faleye <akfaleye@gmail.com>
2023-11-24 04:14:33 +0100

    Define the width and height of the website's logo

--- level-upfront-hackathon-2023/app.js
+++ level-upfront-hackathon-2023/app.js
@@ -1,7 +1,17 @@
-// Scripts
 
 let x = 2
 
 if(true) {
   let x = 3
 }
+
+
+// DOM helpers
+
+/*
+ * getElement - Gets an Element in the DOM.
+ * @selector - a CSS selector, that targets an element in the DOM
+ */
+var getElement = (selector) => document.querySelector(selector);
+
+
