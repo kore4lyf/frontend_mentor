136a3cf1e240a32038022a84f1299299816535a9
Korede Faleye <akfaleye@gmail.com>
2023-11-26 18:15:59 +0100

    Make accordion open and close

--- level-upfront-hackathon-2023/app.js
+++ level-upfront-hackathon-2023/app.js
@@ -5,9 +5,12 @@ if(true) {
   let x = 3
 }
 
+// Accordion 
+const toggleAccordion = (e) => {
+  e.currentTarget.parentElement.classList.toggle("open")
+}
 
 // Helpers
-
 var getElement = (selector) => document.querySelector(selector);
 
 
